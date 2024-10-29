df1d884ddf983f72890f8eab8a611a3b517d5388
Сергей Колесников <skx2013@mail.ru>
2024-10-30 00:34:46 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+namespace TelegramBot
+{
+    class Program
+    {
+        private static readonly string token = "7511970901:AAFKJAWDZZk6cS_6Uy7iDzudGVXSvGq1ZMw";
+        private static ITelegramBotClient? client;
+
+        static async Task Main()
+        {
+            client = new TelegramBotClient(token);
+            client.StartReceiving(UpdateHandler, ErrorHandler);
+
+            var bot = await client.GetMeAsync();
+            Console.WriteLine($"{bot.FirstName} запущен!");
+
+            await Task.Delay(-1);
+        }
+
+        private static async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            try
+            {
+                var message = update.Message;
+                var user = message.From;
+
+                if (message.Text != null)
+                {
+                    Console.WriteLine($"{user.FirstName} ({user.Id}) написал: {message.Text}");
+
+                    if (message.Text.ToLower().Contains("погода"))
+                    {
+                        WeatherService weatherService = new WeatherService();
+                        string weatherInfo = await weatherService.GetWeatherInfo();
+                        await client.SendTextMessageAsync(message.Chat.Id,weatherInfo);
+                        return;
+                    }    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static async Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    public class WeatherService
+    {
+        private static readonly string apiKey = "b2feb79cb307f3c4f040517310e224cc";
+        private static readonly string City = "Berdsk";
+        
+        public async Task<string> GetWeatherInfo()
+        {
+            HttpClient client = new HttpClient();
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={City}&appid={apiKey}&units=metric";
+            string response = await client.GetStringAsync(url);
+
+            var json = JObject.Parse(response);
+            string temp = json["main"]["temp"].ToString();
+            string feels = json["main"]["feels_like"].ToString();
+            string weather = json["weather"][0]["main"].ToString();
+
+            return $"Погода в Бердске: {temp}°C - {weather} \n Ощущается как: {feels}°C";
+        }
+    }
+}
